ce10550109579f9b0683da72b1d6524962903253
akash <akashsingh1@hotmail.com>
2021-03-26 04:19:57 +0530

    setting delete functionality and setting null value of best answer id in question table from the fereignId in database

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -26,13 +26,7 @@ class Answer extends Model
             $answer->question->increment('answers_count');
         });
         static::deleted(function($answer){
-            $question = $answer->question; 
-            $question->decrement('answers_count');
-
-            if($question->best_answer_id === $answer->id){
-                $question->best_answer_id = NULL;
-                $question->save();
-            }
+            $answer->question->decrement('answers_count');
         });
 
     }
@@ -40,10 +34,6 @@ class Answer extends Model
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
-    }
-
-    public function getStatusAttribute(){
-        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 
 }
